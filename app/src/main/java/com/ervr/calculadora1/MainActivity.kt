37129546de43d9c14cba6642a5d1f03ef41beeca
Exequiel Venegas <evenegas4414@gmail.com>
2023-06-17 13:19:52 -0400

    Change to Kotlin

--- app/src/main/java/com/ervr/calculadora1/MainActivity.kt
+++ app/src/main/java/com/ervr/calculadora1/MainActivity.kt
@@ -1,99 +1,86 @@
-package com.ervr.calculadora1;
+package com.ervr.calculadora1
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+class MainActivity : AppCompatActivity() {
+/*
+En Kotlin, las variables pueden ser declaradas como nulas agregando el operador de interrogación (?)
+ después del tipo de dato. En el código de Kotlin, las variables editTextNumber1, editTextNumber2 y
+ textViewResult son declaradas como EditText? y TextView?, lo que significa que pueden contener
+ valores nulos.
+ */
+    private var editTextNumber1: EditText? = null
+    private var editTextNumber2: EditText? = null
+    private var textViewResult: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
-    private EditText editTextNumber1, editTextNumber2;
-    private TextView textViewResult;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editTextNumber1 = findViewById(R.id.editTextNumber1);
-        editTextNumber2 = findViewById(R.id.editTextNumber2);
-        textViewResult = findViewById(R.id.textViewResult);
-
-        Button buttonAdd = findViewById(R.id.buttonAdd);
-        Button buttonSubtract = findViewById(R.id.buttonSubtract);
-        Button buttonMultiply = findViewById(R.id.buttonMultiply);
-        Button buttonDivide = findViewById(R.id.buttonDivide);
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateResult('+');
-            }
-        });
-
-        buttonSubtract.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateResult('-');
-            }
-        });
-
-        buttonMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateResult('*');
-            }
-        });
-
-        buttonDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateResult('/');
-            }
-        });
-
-        Button buttonExit = findViewById(R.id.buttonExit);
-        buttonExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        /*
+        Kotlin proporciona una inferencia de tipo más poderosa en comparación con Java.
+        Por lo tanto, no es necesario especificar explícitamente el tipo de datos al
+        inicializar las variables en Kotlin.
+         */
+        editTextNumber1 = findViewById(R.id.editTextNumber1)
+        editTextNumber2 = findViewById(R.id.editTextNumber2)
+        textViewResult = findViewById(R.id.textViewResult)
+        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        val buttonSubtract = findViewById<Button>(R.id.buttonSubtract)
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        buttonAdd.setOnClickListener {
+            Log.d("MainActivity", "Metodo Suma")
+            calculateResult('+') }
+        buttonSubtract.setOnClickListener {
+            Log.d("MainActivity", "Metodo Resta")
+            calculateResult('-') }
+        buttonMultiply.setOnClickListener {
+            Log.d("MainActivity", "Metodo Multiplicar")
+            calculateResult('*') }
+        buttonDivide.setOnClickListener {
+            Log.d("MainActivity", "Metodo Dividir")
+            calculateResult('/') }
+        val buttonExit = findViewById<Button>(R.id.buttonExit)
+        buttonExit.setOnClickListener { finish() }
     }
 
-    private void calculateResult(char operator) {
-        String value1 = editTextNumber1.getText().toString();
-        String value2 = editTextNumber2.getText().toString();
-
+    private fun calculateResult(operator: Char) {
+        /*
+        En el código de Java, se utiliza la interfaz OnClickListener y se crea una instancia
+        anónima para definir el comportamiento del click listener. En Kotlin, se utilizan
+        las expresiones lambda más concisas para definir los click listeners.
+         */
+        val value1 = editTextNumber1!!.text.toString()
+        val value2 = editTextNumber2!!.text.toString()
+        /*
+        En el código de Kotlin, se utiliza el operador de llamada segura (?.)
+        para acceder a las propiedades o métodos de los objetos potencialmente nulos.
+         */
         if (!value1.isEmpty() && !value2.isEmpty()) {
-            double num1 = Double.parseDouble(value1);
-            double num2 = Double.parseDouble(value2);
-            double result = 0;
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0)
-                        result = num1 / num2;
-                    else
-                        Toast.makeText(getApplicationContext(), "No se puede dividir por cero", Toast.LENGTH_SHORT).show();
-                    break;
+            val num1 = value1.toDouble()
+            val num2 = value2.toDouble()
+            var result = 0.0
+            when (operator) {
+                '+' -> result = num1 + num2
+                '-' -> result = num1 - num2
+                '*' -> result = num1 * num2
+                '/' -> if (num2 != 0.0) result = num1 / num2 else Toast.makeText(
+                    applicationContext,
+                    "No se puede dividir por cero",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-
-            textViewResult.setText("Resultado: " + result);
+            textViewResult!!.text = "Resultado: $result"
+            Log.d("MainActivity", result.toString())
         } else {
-            Toast.makeText(getApplicationContext(), "Ingrese ambos valores", Toast.LENGTH_SHORT).show();
+            Toast.makeText(applicationContext, "Ingrese ambos valores", Toast.LENGTH_SHORT).show()
+            Log.d("MainActivity", "Ingrese ambos valores")
         }
     }
 }
